e7b083d3b112b99eb72bf53bead5c1ab974943d1
Stanislav Chvila <schila@gmail.com>
2021-06-17 15:45:17 +0200

    fix tsmongoose

--- ts-nodejs-mongoose/src/databases/index.ts
+++ ts-nodejs-mongoose/src/databases/index.ts
@@ -4,7 +4,7 @@ import { dbConfig } from '@interfaces/db.interface';
 const { host, port, database }: dbConfig = config.get('dbConfig');
 
 export const dbConnection = {
-  url: `mongodb://${host}:${port}/${database}`,
+  url: `mongodb+srv://standa:rnEZ3rjw3lf9J7Bh@cluster0.5tfx5.mongodb.net/tstest`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
